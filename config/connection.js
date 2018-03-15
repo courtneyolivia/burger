462d305ed7b5612753a765e5b374dcb7ed7d670d
sakiohma <sakiohma@github.com>
2018-03-15 03:40:43 -0400

    added files

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+// Set up MySQL connection.
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'ryoohki8',
+    database: 'burgers_db'
+})
+
+connection.connect(function(err){
+    if(err)throw err;
+    console.log("Connection as id: "+connection.threadid);
+});
+
+// Export connection for ORM 
+module.exports = connection;
---
+++ server.js
@@ -0,0 +1,24 @@
+// Load modules
+var express = require('express');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+app.use(methodOverride('_method'));
+app.engine('handlebars',exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+var routes = require('./controllers/routes.js');
+app.use('/', routes);
+
+// Set port
+var port = 3000;
+app.listen(port);
